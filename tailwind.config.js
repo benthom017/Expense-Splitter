ff31a00dd8e5046983f2c289f035e99a8dc95c6d
benthom017 <bthompson0287@gmail.com>
2025-08-15 22:31:54 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ["Inter", "ui-sans-serif", "system-ui"] },
+    },
   },
   plugins: [],
 }
